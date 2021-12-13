af53d13471391abb678b177d587d2748660f644a
fermuniz83 <fercomobone@hotmail.com>
2021-12-13 20:10:21 -0300

    peças

---
+++ peças.js
@@ -0,0 +1,83 @@
+var listaDePecas = ["Filtro de Ar", "Motor", "Amortecedor"]
+
+
+if (listaDePecas.length < 10) {
+    // É possível cadastrar
+    console.log("É possível cadastrar mais peças")
+} else {
+    console.log("Não tem mais espaço na lista")
+}
+
+
+let peso = 50
+
+if(peso<100) {
+    console.log("A peça deve pesar no mínimo 100g")
+} else {
+    console.log("A peça possui peso adequado")
+}
+
+
+let nomeDePeca = "Disco de freio"
+
+if(nomeDePeca.length>3){
+    console.log("Nome de peça está adequado para cadastro")
+} else {
+    console.log("O nome deve ter mais de 3 caracteres, digite um nome adequado")
+}
+
+
+switch(nomeDePeca.length){
+    Case 0:
+    console.log("O nome da peça nao pode ser vazio")
+    break;
+
+    Case 1:
+    Case 2:
+    Case 3:
+    console.log("O nome deve ter mais de 3 caracteres, digite um nome adequado")
+
+    default:
+        console.log("Nome da peça está adequado")
+}var listaDePecas = ["Filtro de Ar", "Motor", "Amortecedor"]
+
+
+if (listaDePecas.length < 10) {
+    // É possível cadastrar
+    console.log("É possível cadastrar mais peças")
+} else {
+    console.log("Não tem mais espaço na lista")
+}
+
+
+let peso = 50
+
+if(peso<100) {
+    console.log("A peça deve pesar no mínimo 100g")
+} else {
+    console.log("A peça possui peso adequado")
+}
+
+
+let nomeDePeca = "Disco de freio"
+
+if(nomeDePeca.length>3){
+    console.log("Nome de peça está adequado para cadastro")
+} else {
+    console.log("O nome deve ter mais de 3 caracteres, digite um nome adequado")
+}
+
+
+switch(nomeDePeca.length){
+    Case 0:
+    console.log("O nome da peça nao pode ser vazio")
+    break;
+
+    Case 1:
+    Case 2:
+    Case 3:
+    console.log("O nome deve ter mais de 3 caracteres, digite um nome adequado")
+
+    default:
+        console.log("Nome da peça está adequado")
+}
